b943acde98b39ef99b74613d90435375c59d8cde
Nguyen Nam <nguyen.nam@urban-funes.co.jp>
2018-12-20 11:59:38 +0900

    reject PP

--- config/domain/default.php
+++ config/domain/default.php
@@ -36,11 +36,3 @@ if(!defined("AUTO_CLEAR_CACHED")){
 	 */
 	define("AUTO_CLEAR_CACHED", false);
 }
-
-// https://developer.paypal.com/developer/applications/braintreeCredentials/editbtcred?email=bmdvY25hbS5zay1mYWNpbGl0YXRvckBnbWFpbC5jb20=&sbAccountNumber=1850192643725790944
-define("PAYPAL_ACCESS_TOKEN"			, 'access_token$sandbox$vgbyhmjgvchtnr9b$b586e8bb9c4905ecb4f48509549342f8'); // ExpiryDate : 12 Aug 2028
-
-define("PAYPAL_SANDBOX_ACCOUNT"			, 'ngocnam.sk-facilitator@gmail.com');
-define("PAYPAL_CLIENT_ID"				, 'AYqwMDLyN4DYOCAYumdZYIjhVRiKY29Ka9lBJY7YzWV2KBO3nB1bKuY0BCXXMiOdcRSLQcd2NBeobjXv');
-define("PAYPAL_SECRET_KEY"				, 'EJ_NIEYfGumNZQD5WrUXzz7MXve_1_iVlPP-MYx4iqONnfo_tnelwCVUch7UF2JhAKymXJ1dsZFHjbKZ');
-
